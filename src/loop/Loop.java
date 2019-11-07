c3c55e66986456200a6000ce5f55095ec3089a91
Jeyhun Ali <Ali-ceyhun@list.ru>
2019-11-07 16:13:06 -0500

    loop

---
+++ src/loop/Loop.java
@@ -0,0 +1,25 @@
+package loop;
+
+public class Loop {
+
+	public static void main(String[] args) {
+	 
+		
+		for(int i=0; i<5; i++) 
+			System.out.println ("hello");
+		
+			
+for (int i=0; i<6; i++)
+	System.out.println("TT GREAT in quuens");
+
+
+for (int i=0; i<5; i++)
+System.out.println("Knowledge is Power");
+
+
+for (int i=5; i<15; i++)
+	System.out.println("one apple a day - no doctor");
+
+	}
+  
+}
